7bb447a08609c070dc5612aef95072d9603655d8
Justin <justin@mifko.co>
2024-02-01 22:26:51 -0500

    fixes ddns.sh

--- cloudflare/ddns.sh
+++ cloudflare/ddns.sh
@@ -13,6 +13,8 @@ zone=mifko.co
 # DNS records to be updated
 #dnsrecords=(vpn.mifko.co vcenter.mifko.co desktop.mifko.co www.mifko.co mifko.co)
 dnsrecords=(mifko.co)
+email=jmifkovich@gmail.com
+auth_key={api-key}
 
 # Flag for Cloudflare proxy status (ALL LOWER CASE)
 use_proxy=true
@@ -37,21 +39,30 @@ for dnsrecord in "${dnsrecords[@]}"; do
           -H "Authorization: Bearer $cloudflare_auth_key" \
           -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
 
+        echo "cloudflare_zone_id: $cloudflare_zone_id"
+
         cloudflare_dnsrecord=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$cloudflare_zone_id/dns_records?type=A&name=$dnsrecord" \
           -H "Authorization: Bearer $cloudflare_auth_key" \
           -H "Content-Type: application/json")
 
         cloudflare_dnsrecord_ip=$(echo $cloudflare_dnsrecord | jq -r '{"result"}[] | .[0] | .content')
         cloudflare_dnsrecord_proxied=$(echo $cloudflare_dnsrecord | jq -r '{"result"}[] | .[0] | .proxied')
+        cloudflare_dnsrecord_id=$(echo $cloudflare_dnsrecord | jq -r '{"result"}[] | .[0] | .id')
+
+        echo "cloudflare_dnsrecord_id:$cloudflare_dnsrecord_id"
+
+
 
         if [[ "$current_ip" == "$cloudflare_dnsrecord_ip" ]] && [[ "$cloudflare_dnsrecord_proxied" == "$use_proxy" ]]; then
             echo "$dnsrecord DNS record is up to date"
         else
-            cloudflare_dnsrecord_id=$(echo $cloudflare_dnsrecord | jq -r '{"result"}[] | .[0] | .id')
-            # update the record
-            curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$cloudflare_zone_id/dns_records/$cloudflare_dnsrecord_id" \
+
+
+            curl -s -Z PUT "https://api.cloudflare.com/client/v4/zones/$cloudflare_zone_id/dns_records/$cloudflare_dnsrecord_id" \
               -H "Authorization: Bearer $cloudflare_auth_key" \
               -H "Content-Type: application/json" \
+              -H "X-Auth-Email: $email" \
+              -H "X-Auth-Key: $auth_key" \
               --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$current_ip\",\"ttl\":1,\"proxied\":$use_proxy}" | jq
             echo "$dnsrecord DNS record has been updated with the current IP"
         fi
